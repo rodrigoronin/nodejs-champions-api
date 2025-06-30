6b4588a5db64d92850c08dc9f3dccc4b3c8dc778
rodrigoronin <liramailbox@gmail.com>
2025-06-29 21:06:33 -0300

    feat: added Repository layer
    repository layer with basic configuration, no databases yet

---
+++ src/repositories/players-repository.ts
@@ -0,0 +1,23 @@
+interface PlayerModel {
+  id: number;
+  name: string;
+}
+
+const database: PlayerModel[] = [
+  {
+    id: 1,
+    name: 'Neymar'
+  },
+  {
+    id: 2,
+    name: 'Bruxo'
+  }
+];
+
+export const findAllPlayers = async (): Promise<PlayerModel[]> => {
+  return database;
+};
+
+export const findPlayerbyId = async (id: number): Promise<PlayerModel | undefined> => {
+  return database.find((player) => player.id === id);
+};
